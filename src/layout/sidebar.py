4d51b4490df4ac6272ee8fc43af69539e87db024
daiyabarus <daiyabarus@gmail.com>
2024-06-16 16:14:13 +0700

    modified:   src/layout/sidebar.py

--- src/layout/sidebar.py
+++ src/layout/sidebar.py
@@ -7,20 +7,12 @@ def sidebar():
     # Connect to the database
     db_utils = DatabaseUtils()
     db_utils.connect_to_database()
-
-    # Select a table from the database
     selected_table = db_utils.select_table()
-
-    # Get the selected table as a DataFrame
     df = db_utils.get_table_dataframe()
 
     if df is not None:
-        # Display the DataFrame
-        st.dataframe(df)
-
         filter = DynamicFilters(df, filters=["siteid", "neid", "Band Type"])
         with st.sidebar:
             filter.display_filters()
         filter.display_df()
-    # Disconnect from the database
     db_utils.disconnect_from_database()
